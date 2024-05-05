26a803d5214a8b186e71d902d918018507b551d0
maruf-hussain <engmarufhussain@gmail.com>
2024-05-06 04:13:52 +0600

    test

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-red-900">Add Coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/Components/UpdateCoffee.jsx
@@ -0,0 +1,10 @@
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-red-900">Update Coffee</h1>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
